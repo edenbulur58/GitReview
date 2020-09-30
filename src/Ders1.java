8398cc3ef7fa14f338039a464c1df67d77d50ebf
hamza <hyilmaz.techproed@gmail.com>
2020-09-30 18:06:15 +0300

    feature branchte commit

--- src/Ders1.java
+++ src/Ders1.java
@@ -7,7 +7,7 @@ public class Ders1 {
 		System.out.println("Saat 18:46");
 		System.out.println("Saat 19:17 - iskender");
 		System.out.println("Saat 19:28 - hamza conflict");
-
+		System.out.println("Saat 18:05 - feature branch 1. satýr");
 		
 	}
 
